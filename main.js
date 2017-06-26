aa048c7a99d96d959db10146bda626b0cda340f5
Andrea Parodi <andrea@parro.it>
2017-06-26 22:53:15 +0200

    skeleton

--- main.js
+++ main.js
@@ -1,3 +1,87 @@
-export default function keyboardeventMatchElectronAccelerator() {
-	return 42;
+
+const modifiers = /^(Command|Cmd|Control|Ctrl|CommandOrControl|CmdOrCtrl|Alt|Option|AltGr|Shift|Super)/i;
+const keyCodes = /^([0-9A-Z)!@#$%^&*(:+<_>?~{|}";=,\-./`[\\\]']|F1*[1-9]|F10|F2[0-4]|Plus|Space|Tab|Backspace|Delete|Insert|Return|Enter|Up|Down|Left|Right|Home|End|PageUp|PageDown|Escape|Esc|VolumeUp|VolumeDown|VolumeMute|MediaNextTrack|MediaPreviousTrack|MediaStop|MediaPlayPause|PrintScreen)/i;
+
+export function reduceModifier({accelerator, event}, modifier) {
+	switch (modifier) {
+		case 'command':
+		case 'cmd': {
+			return {
+				event: Object.assign({}, event, {meta: true}),
+				accelerator: accelerator.slice(modifier.length)
+			};
+		}
+		case 'control':
+		case 'ctrl': {
+			return {
+				event: Object.assign({}, event, {ctrl: true}),
+				accelerator: accelerator.slice(modifier.length)
+			};
+		}
+		case 'commandorcontrol': {
+			break;
+		}
+		case 'cmdorctrl': {
+			break;
+		}
+		case 'alt': {
+			break;
+		}
+		case 'option': {
+			break;
+		}
+		case 'altgr': {
+			break;
+		}
+		case 'shift': {
+			break;
+		}
+		case 'super': {
+			break;
+		}
+		default:
+	}
+}
+
+export function reducePlus({accelerator, event}) {
+	return {
+		event,
+		accelerator: accelerator.trim().slice(1)
+	};
+}
+
+export function reduceKeyCode({accelerator, event}, code) {
+	return {
+		event: Object.assign({}, event, {code}),
+		accelerator: accelerator.trim().slice(code.length)
+	};
+}
+
+export function toKeyEvent(accelerator) {
+	let state = {accelerator, event: {}};
+	while (state.accelerator !== '') {
+		const modifierMatch = modifiers.match(accelerator);
+		if (modifierMatch) {
+			const modifier = modifierMatch[0].toLowerCase();
+			state = reduceModifier(state, modifier);
+		}
+
+		if (state.accelerator.trim()[0] === '+') {
+			state = reducePlus(state);
+		}
+
+		const keyCodeMatch = keyCodes.match(accelerator);
+		if (keyCodeMatch) {
+			const keyCode = keyCodeMatch[0].toLowerCase();
+			state = reduceKeyCode(keyCode);
+		}
+	}
+	return state.event;
+}
+
+export function match(accelerator, keyEvent) {
+	if (toKeyEvent(accelerator).code === keyEvent.code) {
+		return true;
+	}
+	return false;
 }
--- test.js
+++ test.js
@@ -1,6 +1,56 @@
 import test from 'ava';
-import keyboardeventMatchElectronAccelerator from '.';
+import {match, reduceModifier, reducePlus, reduceKeyCode} from '.';
 
 test('exports a function', t => {
-	t.is(typeof keyboardeventMatchElectronAccelerator, 'function');
+	t.is(typeof match, 'function');
+});
+
+test('handle ctrl', t => {
+	const newState = reduceModifier({
+		accelerator: 'ctrl+c',
+		event: {}
+	}, 'ctrl');
+
+	t.deepEqual(newState, {
+		accelerator: '+c',
+		event: {ctrl: true}
+	});
+});
+
+test('handle control', t => {
+	const newState = reduceModifier({
+		accelerator: 'control+c',
+		event: {}
+	}, 'control');
+
+	t.deepEqual(newState, {
+		accelerator: '+c',
+		event: {ctrl: true}
+	});
+});
+
+test('handle plus', t => {
+	const event = {};
+	const newState = reducePlus({
+		accelerator: '+c',
+		event
+	}, 'control');
+
+	t.deepEqual(newState, {
+		accelerator: 'c',
+		event
+	});
+	t.is(event, newState.event);
+});
+
+test('handle keyCode', t => {
+	const newState = reduceKeyCode({
+		accelerator: 'c',
+		event: {}
+	}, 'c');
+
+	t.deepEqual(newState, {
+		accelerator: '',
+		event: {code: 'c'}
+	});
 });
