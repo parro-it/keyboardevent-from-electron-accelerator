5aecaa76cfa5b55a7407cebe8772f7c5bcc86714
Andrea Parodi <andrea@parro.it>
2017-06-27 21:01:59 +0200

    example

--- example.js
+++ example.js
@@ -1,4 +1,5 @@
-import keyboardeventMatchElectronAccelerator from 'keyboardevent-match-electron-accelerator';
-
-console.log({keyboardeventMatchElectronAccelerator});
+const {toKeyEvent} = require('keyboardevent-from-electron-accelerator');
+console.log(toKeyEvent('Ctrl+Shift+V'));
+console.log(toKeyEvent('Control+c'));
+console.log(toKeyEvent('Alt+Delete'));
 
